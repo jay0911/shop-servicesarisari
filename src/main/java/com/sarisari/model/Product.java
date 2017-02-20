c43dbb27e2c6106fbe62461f59d79abda11969f6
jaypogi69 <jayoliveros286@gmail.com>
2017-02-21 01:27:23 +0800

    add model mapping hibernate

--- src/main/java/com/sarisari/model/Product.java
+++ src/main/java/com/sarisari/model/Product.java
@@ -1,5 +1,101 @@
 package com.sarisari.model;
 
-public class Product {
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_product")
+public class Product {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "productid")
+	private int productid;
+	
+	@Column(name = "productname")
+	private String productname;
+	
+	@Column(name = "quantity")
+	private int quantity;
+	
+	@Column(name = "details")
+	private String details;
+	
+	@Column(name = "productimagepath")
+	private String productimagepath;
+	
+	@Column(name = "productprice")
+	private double productprice;
+	
+    @ManyToOne
+    @JoinColumn(name = "storeid")
+	private StoreOwner storeOwner;
+	
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "tbl_orderlines",
+            joinColumns = @JoinColumn(name = "productid"),
+            inverseJoinColumns = @JoinColumn(name = "transactionid")
+    )
+	private Set<Transaction> transaction;
+	
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public String getProductimagepath() {
+		return productimagepath;
+	}
+	public void setProductimagepath(String productimagepath) {
+		this.productimagepath = productimagepath;
+	}
+	public double getProductprice() {
+		return productprice;
+	}
+	public void setProductprice(double productprice) {
+		this.productprice = productprice;
+	}
+	public StoreOwner getStoreOwner() {
+		return storeOwner;
+	}
+	public void setStoreOwner(StoreOwner storeOwner) {
+		this.storeOwner = storeOwner;
+	}
+	public Set<Transaction> getTransaction() {
+		return transaction;
+	}
+	public void setTransaction(Set<Transaction> transaction) {
+		this.transaction = transaction;
+	}
+	
 }
--- src/main/java/com/sarisari/model/StoreOwner.java
+++ src/main/java/com/sarisari/model/StoreOwner.java
@@ -1,5 +1,77 @@
 package com.sarisari.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_store")
 public class StoreOwner {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "storeid")
+	private int storeid;
+	
+	@Column(name = "details")
+	private String details;
+	
+	@Column(name = "name")
+	private String name;
+	
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userid")
+	private User user;
+	
+    @OneToMany(mappedBy = "tbl_store", cascade = CascadeType.ALL)
+	private Set<Product> product;
+    
+    @OneToMany(mappedBy = "tbl_store", cascade = CascadeType.ALL)
+    private Set<Transaction> transaction;
+	
+	public int getStoreid() {
+		return storeid;
+	}
+	public void setStoreid(int storeid) {
+		this.storeid = storeid;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Set<Product> getProduct() {
+		return product;
+	}
+	public void setProduct(Set<Product> product) {
+		this.product = product;
+	}
+	public Set<Transaction> getTransaction() {
+		return transaction;
+	}
+	public void setTransaction(Set<Transaction> transaction) {
+		this.transaction = transaction;
+	}
 
 }
