2dd6cb1854b71f3abba81b837ca11d57c81c6b31
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-02-27 19:58:08 +0800

    add shop maintenance registration

---
+++ src/main/java/com/sarisari/dto/UserMaintenanceDTO.java
@@ -0,0 +1,106 @@
+package com.sarisari.dto;
+
+public class UserMaintenanceDTO {
+	private String fullname;
+	private String contactnumber;
+	private String emailaddress;
+	private String address;
+	private String gender;
+	private String username;
+	private String password;
+	private int points;
+	private String usergroup;
+	private int storeid;
+	private String storename;
+	private String storedetails;
+	
+	public UserMaintenanceDTO(){}
+	
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getContactnumber() {
+		return contactnumber;
+	}
+	public void setContactnumber(String contactnumber) {
+		this.contactnumber = contactnumber;
+	}
+	public String getEmailaddress() {
+		return emailaddress;
+	}
+	public void setEmailaddress(String emailaddress) {
+		this.emailaddress = emailaddress;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString(){
+		return fullname + " " + contactnumber + " " + emailaddress + " " + address+ " " +gender+ " " + username+ " " + password;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public String getUsergroup() {
+		return usergroup;
+	}
+
+	public void setUsergroup(String usergroup) {
+		this.usergroup = usergroup;
+	}
+
+	public int getStoreid() {
+		return storeid;
+	}
+
+	public void setStoreid(int storeid) {
+		this.storeid = storeid;
+	}
+
+	public String getStorename() {
+		return storename;
+	}
+
+	public void setStorename(String storename) {
+		this.storename = storename;
+	}
+
+	public String getStoredetails() {
+		return storedetails;
+	}
+
+	public void setStoredetails(String storedetails) {
+		this.storedetails = storedetails;
+	}
+}
